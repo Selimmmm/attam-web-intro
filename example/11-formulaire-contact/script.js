d6a4dc60bd628816e64a14e188bc62b3a7d0fef5
Selimmmm <selim.mellouk.halgand@proton.me>
2026-06-05 11:37:43 +0200

    exemple commented

--- example/11-formulaire-contact/script.js
+++ example/11-formulaire-contact/script.js
@@ -1,14 +1,25 @@
+// On récupère les éléments 'formulaire' et 'confirmation'
 const form = document.querySelector("#contact");
 const confirmation = document.querySelector("#confirmation");
 
+// On 'écoute' le formulaire 
+// (seulement l'event 'submit' = cliquer sur un bouton avec type="submit")
 form.addEventListener("submit", (event) => {
-  event.preventDefault(); // empêche le rechargement de la page
 
+  event.preventDefault(); // empêche le rechargement de la page (qui est auto avec type="submit")
+
+  // On sélectionne les valeurs de nom, d'email et de message qui ont été complétées
   const nom = document.querySelector("#nom").value;
   const email = document.querySelector("#email").value;
   const message = document.querySelector("#message").value;
 
+  // Ligne de test : 
+  // On vérifie : 
+  // - que le nom n'est pas vide
+  // - que l'email contient un symbole '@'
+  // - que le message n'est pas vide
   if (!nom || !email.includes("@") || !message) {
+    // On change le message de confirmation
     confirmation.textContent = "Remplis tous les champs avec un e-mail valide.";
     return;
   }
